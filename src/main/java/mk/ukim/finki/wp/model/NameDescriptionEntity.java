514b5545eebd16c7f78c8751e949045419cfaf1d
ristes <ristes@github.com>
2015-02-09 17:02:59 +0100

    - git ignore - Column length declaration for description in NameDescriptionEntity

--- src/main/java/mk/ukim/finki/wp/model/NameDescriptionEntity.java
+++ src/main/java/mk/ukim/finki/wp/model/NameDescriptionEntity.java
@@ -1,10 +1,12 @@
 package mk.ukim.finki.wp.model;
 
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public class NameDescriptionEntity extends NamedEntity {
 
+	@Column(length = 1500)
 	private String description;
 
 	public String getDescription() {
