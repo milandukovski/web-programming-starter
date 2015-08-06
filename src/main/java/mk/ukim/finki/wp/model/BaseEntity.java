979487a4251c6555ad441be705778c03da03609b
ristes <ristes@gmail.com>
2015-08-06 17:53:14 +0200

    event entity formatting base entity json ignores unknown properties

--- src/main/java/mk/ukim/finki/wp/model/BaseEntity.java
+++ src/main/java/mk/ukim/finki/wp/model/BaseEntity.java
@@ -1,56 +1,57 @@
 package mk.ukim.finki.wp.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 /**
  * Simple JavaBean domain object with an id property. Used as a base class for
  * objects needing this property.
- * 
+ *
  * @author Tomche Delev
- * 
  */
 @MappedSuperclass
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BaseEntity {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	protected Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  protected Long id;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-	public Long getId() {
-		return id;
-	}
+  public Long getId() {
+    return id;
+  }
 
-	@JsonIgnore
-	public boolean isNew() {
-		return (this.id == null);
-	}
+  @JsonIgnore
+  public boolean isNew() {
+    return (this.id == null);
+  }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (!(obj instanceof BaseEntity)) {
-			return false;
-		}
-		BaseEntity be = (BaseEntity) obj;
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof BaseEntity)) {
+      return false;
+    }
+    BaseEntity be = (BaseEntity) obj;
 
-		if (this.getId() == null || be.getId() == null) {
-			return super.equals(obj);
-		} else {
-			return this.getId().equals(be.getId());
-		}
-	}
+    if (this.getId() == null || be.getId() == null) {
+      return super.equals(obj);
+    } else {
+      return this.getId().equals(be.getId());
+    }
+  }
 
-	@Override
-	public int hashCode() {
-		// TODO Auto-generated method stub
-		return this.id != null ? (this.getClass() + "-" + this.id).hashCode()
-				: super.hashCode();
-	}
+  @Override
+  public int hashCode() {
+    // TODO Auto-generated method stub
+    return this.id != null ? (this.getClass() + "-" + this.id).hashCode()
+      : super.hashCode();
+  }
 }
